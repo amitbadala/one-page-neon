c2119c01f627429d433a5d7fbd96e467583c5fe7
Parth Desai <desaiparth2000@gmail.com>
2022-07-11 22:35:46 +0530

    modified:   gatsby-config.js 	modified:   src/pages/index.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Parth Desai',
+    title: 'Portfolio | Parth Desai',
     description:
       'I am a Data Engineer who is passionate about Data Science and Automation. In my free time, I like developing.',
     siteUrl: 'https://parthdesai.vercel.app', // No trailing slash allowed!
